8a37d8337fa042bca059089e9cd7a2a68e11dea9
leronbergelson <133677161+CFIALeronB@users.noreply.github.com>
2023-07-05 15:14:30 -0400

    Update Home.js
    - Adding fetchData function to Home.js which sends out a POST request containing an empty query.
    - If process.env.REACT_APP_BACKEND_URL is null or if the response is invalid then error messages are thrown.

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,9 +2,41 @@ import { SearchBar } from '../../components/SearchBar';
 import styles from "../home/Home.module.css";
 import Header from '../../components/Header';
 import CFIALogo from '../../components/CFIALogo';
+import { useEffect } from "react";
 
+// Function to fetch data from the API
+export const fetchData = async () => {
+    try{
+        const response = await fetch(process.env.REACT_APP_BACKEND_URL, {
+            method: "POST",
+            headers: {
+                'Content-Type':'application/json'
+            },
+            body: JSON.stringify({
+                query: ""
+            }),
+        });
+
+        if(response.ok) {
+            console.log("Active Server Connection");
+        } else{
+            throw new Error("Request failed");
+        }
+    }catch (error){
+        console.error("Error: ", error);
+    }
+}
 
 const Home = () => {
+
+    useEffect(() => {
+        // Check if the REACT_APP_BACKEND_URL is set
+        if(process.env.REACT_APP_BACKEND_URL){
+            fetchData();
+        } else{
+            console.error('The REACT_APP_BACKEND_URL environment variable is not set or empty.');
+        }
+    })
 
     return (
         <div className={styles.layout}>
