b24cf09d5d3dee123e3d0088ed6a468823d96686
CFIALeronB <Leron.Bergelson@inspection.gc.ca>
2023-07-13 14:52:01 -0400

    Implementing function to generate the endpoint URL based on the path.

--- src/api/useApiUtil.js
+++ src/api/useApiUtil.js
@@ -1,69 +1,67 @@
 import { useState, useEffect } from "react";
 
+// Function to generate the endpoint URL based on the path
+export const GetEndpoint = (path) => {
+    return process.env.REACT_APP_BACKEND_URL + path;
+};
+
+// Custom hook for making API requests
 export const useApiUtil = (term) => {
     const [data, setData] = useState(null);
 
     useEffect(() => {
-
+        // Fetch data function
         const fetchData = async () => {
-
-            try{        
-                const response = await fetch(process.env.REACT_APP_BACKEND_URL + "/search", {
+            try {
+                const response = await fetch(GetEndpoint("/search"), {
                     method: "POST",
-                    headers: {
-                        'Content-Type':'application/json'
-                    },
+                    headers: { 'Content-Type': 'application/json' },
                     body: JSON.stringify({
                         query: term
-                    }),
-                });
+                      }),
+                    });
 
                 if(response.ok) {
                     const responseData = await response.json();
                     setData(responseData);
                     console.log(responseData);
                     console.log("This is the data: " + data);
-
                 } else {
                     throw new Error("Request failed");
                 }
-
             } catch(error) {
-                console.error("Error: ", error);
+            console.error("Error: ", error);
             }
-        }
+        };
 
-        console.log(term);
-        fetchData();
-
+    console.log(term);
+    fetchData();
+    
     })
 
-    return { data };
-}
+  return { data };
+};
 
+// Function to ping the backend
 export const PingBackend = async () => {
     try {
-      const response = await fetch(process.env.REACT_APP_BACKEND_URL + "/search", {
+        const response = await fetch(GetEndpoint("/search"), {
         method: "POST",
-        headers: {
-          'Content-Type': 'application/json'
-        },
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
-          query: ""
-        }),
-      });
-  
-      if(response.ok) {
-        console.log("Active Server Connection");
-        const data = await response.json(); // Parse the response body
-        return data; // Return the response data
-      } else {
-        throw new Error("Warning: Initializing ping request to backend $REACT_APP_BACKEND_URL failed.");
-      }
-  
+            query: ""
+          }),
+        });
+
+        if(response.ok) {
+            console.log("Active Server Connection");
+            const data = await response.json(); // Parse the response body
+            return data; // Return the response data
+        } else {
+            throw new Error("Warning: Initializing ping request to backend $REACT_APP_BACKEND_URL failed.");
+        }
     } catch(error) {
-      console.error("Error: ", error);
-      throw error; // Re-throw the error
+        console.error("Error: ", error);
+        throw error; // Re-throw the error
     }
-  }
-  
+};
